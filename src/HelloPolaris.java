4a55a0e852b3f5d37fab8f2b94b2488793f07ebc
Seth Gardner <seth.gardner.a@gmail.com>
2021-07-29 15:53:28 -0500

    practiced the different primitive types and strings

--- src/HelloPolaris.java
+++ src/HelloPolaris.java
@@ -1,5 +1,14 @@
 public class HelloPolaris {
     public static void main(String[] args){
         System.out.println("Hello, World!");
+        System.out.println("help");
+
+        int myFavoriteNumber = 8;
+        System.out.println(myFavoriteNumber);
+        String myString = "This string is mine, there are many like it but this one is mine";
+        System.out.println(myString);
+        float myNumber;
+        myNumber = (int) 3.14;
+        System.out.println(myNumber);
     }
 }
