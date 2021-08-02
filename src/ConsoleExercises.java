c424172d8db1a1164858852a1fac03faebd9b3f7
Seth Gardner <seth.gardner.a@gmail.com>
2021-08-02 17:18:06 -0500

    java practice

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class ConsoleExercises {
+    public static void main(String[] args) {
+//        double pi = 3.14159;
+//        System.out.format("%.2f", pi);
+        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Enter an int: ");
+//        int userInt = scanner.nextInt();
+//        System.out.println(userInt);
+//        System.out.print("Enter three words: ");
+//        String[] words = scanner.nextLine().split(" ");
+//        System.out.println(words[0]);
+//        System.out.println(words[1]);
+//        System.out.println(words[2]);
+//        System.out.print("Enter the length and width of the room: ");
+//        String[] dimensions = scanner.nextLine().split(" ");
+//        float length = Float.parseFloat(dimensions[0]);
+//        float width = Float.parseFloat(dimensions[1]);
+//        System.out.println("The perimeter is: " + (length * 2 + width * 2));
+//        System.out.println("The Area is: " + (length * width));
+//        int i = 0;
+//        while (i <= 15){
+//            System.out.println(i);
+//            i++;
+//        }
+//        do{
+//
+//        }while(i < 3){return 0};
+//
+    }
+}
---
+++ src/ControlFlowExercise.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+public class ControlFlowExercise {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+//        int i = 5;
+//        while (i <= 15){
+//            System.out.println(i);
+//            i++;
+//        }
+//        int i = 0;
+//        do{
+//            System.out.println(i);
+//            i += 2;
+//        }while(i < 100);
+//        do{
+//            System.out.println(i);
+//            i -= 5;
+//        }while (i >= -10);
+//        long i = 2;
+//        do{
+//            System.out.println(i);
+//            i = i * i;
+//        }while(i < 1000000);
+//        for (long i = 2; i < 1000000; i+=0) {
+//            System.out.println(i);
+//            i *= i;
+//        }
+//        for (int i = 1; i <= 100; i++) {
+//            if (i % 3 == 0 && i % 5 == 0){
+//                System.out.println("FizzBuzz");
+//                continue;
+//            }else if(i % 3 == 0){
+//                System.out.println("Fizz");
+//                continue;
+//            }else if (i % 5 == 0){
+//                System.out.println("Buzz");
+//                continue;
+//            }
+//            System.out.println(i);
+//        }
+//        System.out.print("What Number would you like to go up: ");
+//        int highestNumber = scanner.nextInt();
+//        System.out.println("number | squared | cubed");
+//        System.out.println("------ | ------- | -----");
+//        for (long i = 0; i <= highestNumber; i++){
+//            System.out.println(i + "      | " + (i * i) + "       | " + (i * i * i));
+//        }
+
+    }
+}
