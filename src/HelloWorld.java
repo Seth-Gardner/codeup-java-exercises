66274d76254605dcf5a0620a9369851644d6f59e
Seth Gardner <seth.gardner.a@gmail.com>
2021-08-06 09:59:45 -0500

    pushing chagnes from yesterday

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,20 +2,20 @@ import java.util.Scanner;
 
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("Hello, World!");
-        int myFavoriteNumber = 8;
-        String myString = "bruh";
-        System.out.println(myString);
-        float myNumber = 3.14f;
-        System.out.println(myNumber);
+//        System.out.println("Hello, World!");
+//        int myFavoriteNumber = 8;
+//        String myString = "bruh";
+//        System.out.println(myString);
+//        float myNumber = 3.14f;
+//        System.out.println(myNumber);
 
 //        int x = 5;
 //        System.out.println(x++);
 //        System.out.println(x);
 
-        int x = 5;
-        System.out.println(++x);
-        System.out.println(x);
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
 
 //        String theNumberThree = "three";
 //        Object o = theNumberThree;
--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,4 +1,6 @@
+import java.util.Scanner;
 public class MethodsExercises {
+    Scanner scanner = new Scanner(System.in);
 
     public static double Addition(double num1, double num2){
         return num1 + num2;
@@ -25,6 +27,22 @@ public class MethodsExercises {
         return num1 % num2;
     }
 
+    public static int GetInteger(int min, int max){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number between " + min + " and " + max + ": ");
+        int number = scanner.nextInt();
+        if(number >= min && number <= max){
+            return number;
+        }else{return GetInteger(min, max);}
+    }
+    public static long Factorial(int num){
+        long total = 1;
+        for (int i = 0; i < num; i++) {
+            total *= num - i;
+        }
+        return total;
+    }
+
 
     public static void main(String[] args) {
         System.out.println(Addition(500.54, 3.1415926535));
@@ -32,5 +50,7 @@ public class MethodsExercises {
         System.out.println(Multiplication(5, 5));
         System.out.println(Division(555, 1));
         System.out.println(Modulus(500, 34.33546345));
+//        System.out.println(GetInteger(1, 10));
+        System.out.println(Factorial(20));
     }
 }
