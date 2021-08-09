662ca1e475aa93738cf8bbd73bcc944577637488
Seth Gardner <seth.gardner.a@gmail.com>
2021-08-09 15:05:14 -0500

    removed code from rectangle and square as per instructions

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,22 +1,5 @@
 package shapes;
 
 public class Rectangle {
-    protected int length;
-    protected int width;
-
-    public Rectangle(int length, int width) {
-        this.length = length;
-        this.width = width;
-    }
-
-    //  returns the area of a rectangle
-    public int getArea(){
-        return length * width;
-    }
-
-//  returns the perimeter of a rectangle
-    public int getPerimeter(){
-        return 2 * length + 2 * width;
-    }
 
 }
